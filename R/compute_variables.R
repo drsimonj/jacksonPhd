a6653bb7f3bc986619143d309ae8bebfa6f3d74e
drsimonj <drsimonjackson@gmail.com>
2016-02-19 13:41:04 +1100

    Added post anbd discrimination to compute_variable functions

--- R/compute_variables.R
+++ R/compute_variables.R
@@ -25,3 +25,49 @@ computeAccuracy <- function(x, na.rm = TRUE) {
 computeConfidence <- function(x, na.rm = TRUE) {
   rowMeans(x[, getCol(x, "c")], na.rm)
 }
+
+#' Compute the POST (Point Of Sufficient cerTainty) for each participant in Jackson Phd test data frame.
+#'
+#' @param x Data frame from phd sample.
+#' @param na.rm logical. Should missing values (including NaN) be omitted from the calculations? Default = True
+#' @return POST vector. Any values < 0 or > 100 scored as NA.
+#' @examples
+#' computePost(phd[[1]]$EA)
+computePost <- function(x) {
+  dcs <- getCol(x, "d")
+  crs <- getCol(x, "c")
+
+  posts <- apply(x, 1, function(i) {
+    fit  <- glm(i[dcs] ~ i[crs], family = binomial())
+    -coef(fit)[[1]] / coef(fit)[[2]]
+  })
+
+  posts[is.na(posts) | posts < 0 | posts > 100] <- NA
+
+  posts
+}
+
+#' Compute discrimination (confidence for correct answers - confidence for
+#' incorrect answers) for each participant in Jackson Phd test data frame.
+#'
+#' @param x Data frame from phd sample.
+#' @param na.rm logical. Should missing values (including NaN) be omitted from the calculations? Default = True
+#' @return Discrimination vector.
+#' @examples
+#' computeDiscrimination(phd[[1]]$EA)
+computeDiscrimination <- function(x, na.rm = TRUE) {
+  acc <- getCol(x, "a")
+  crs <- getCol(x, "c")
+
+  tmp <- sapply(c(1, 0), function(a) {
+    apply(x, 1, function(i) {
+      pick <- i[acc] == a
+      if (any(pick)) {
+        return (mean(i[crs][pick], na.rm = na.rm))
+      } else {
+        return (NA)
+      }
+    })
+  })
+  tmp[, 1] - tmp[, 2]
+}
